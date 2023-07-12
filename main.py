688d3c58667785a5af8d197ffcd8db4e60aa4f18
Röttger <57038059+MRoettger@users.noreply.github.com>
2023-07-12 10:19:41 +0200

    update main

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 # Y = 2*X - 1
-model.fit(xs, ys, epochs=3000)
+model.fit(xs, ys, epochs=300)
 
 # Vorhersage für den Wert 10
 print(model.predict([10.0]))
 print("Here is what I learned: {}".format(l0.get_weights()))
+
+text = "Model prediction was: " + str(model.predict([10.0]) ) + "\nHere is what I learned: {}".format(l0.get_weights())
+f = open("result.txt", "w")
+f.write(text)
+f.close()
