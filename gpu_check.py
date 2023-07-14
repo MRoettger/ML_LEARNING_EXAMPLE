3259e0db4dd5a3e25426b878f062a6cace0052e9
Röttger <57038059+MRoettger@users.noreply.github.com>
2023-07-14 11:18:45 +0200

    update file

--- gpu_check.py
+++ gpu_check.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
 
-print(tf.reduce_sum(tf.random.normal([1000, 1000])))
 print(tf.config.list_physical_devices('GPU'))
+
+print("\n \n :")
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+
